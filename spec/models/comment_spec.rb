1634f2b170af1cc7f1ee31a6e6874c508a5bee12
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-10-19 17:52:09 -0500

    comment-model rspec passing

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
-
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe 'comments' do
+		before do
+			@user = create_user
+			@message = @user.messages.create(content: 'message')
+			@comment = Comment.create(content: 'comment', user: @user, message: @message)
+		end
+		it 'belongs to a user' do
+			expect(@comment.user).to eq(@user)
+		end
+		it 'belongs to a message' do
+			expect(@comment.message).to eq(@message)
+		end
+	end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,26 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+
+def create_user first_name='joe', last_name='doe', email='JoeBlow@email.com', password='password', password_confirmation='password'
+  User.create(first_name: first_name, last_name: last_name, email: email, password: password, password_confirmation: password_confirmation)
+end
+def log_in user, password='password'
+  visit '/sessions/new'
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: password
+  click_button 'Log In'
+end
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
+
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
