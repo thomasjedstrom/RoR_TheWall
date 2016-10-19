1e22cc38b0aff36c2d06e3826d59202690058ab1
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-10-19 17:54:04 -0500

    message-model rspec passing

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe 'messages' do
+		before do
+			@user = create_user
+			@message = @user.messages.create(content: 'message')
+		end
+		it 'belongs to a user' do
+			expect(@message.user).to eq(@user)
+		end
+	end
 end
