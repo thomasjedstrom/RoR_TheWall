35777ce11e78ab84b94ae631fba660f8c344694e
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-10-19 22:03:54 -0500

    messages-controller rspec passing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,13 @@
 class MessagesController < ApplicationController
+	before_action :require_login
   def create
   	message = Message.create(content: params[:content], user: current_user)
   	redirect_to "/users/#{current_user.id}"
   end
 
   def destroy
-  	Message.destroy(params[:id])
+  	message = Message.find(params[:id])
+	message.destroy if message.user == current_user
 	redirect_to "/users/#{current_user.id}"  	
   end
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
-
+  before do
+    @user = create_user
+    @message = @user.messages.create(content: "message")
+  end
+  describe "when not logged in" do
+    before do
+      session[:user_id] = nil
+    end
+    it "cannot access create" do
+      post :create
+      expect(response).to redirect_to(root_path)
+    end
+    it "cannot access destroy" do
+      delete :destroy, id: @message
+      expect(response).to redirect_to(root_path)
+    end
+  end
+  describe "when logged in as the wrong user" do
+    before do
+      @wrong_user = create_user 'jane', 'smith', 'julius@lakers.com'
+      session[:user_id] = @wrong_user.id
+      @message = @user.messages.create(content: 'message')
+    end
+    it "cannot access destroy" do
+      delete :destroy, id: @message, user_id: @user
+      expect(response).to redirect_to("/users/#{@wrong_user.id}")
+    end
+  end
 end
