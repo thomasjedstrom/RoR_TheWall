2239046b996b91ea6c82ca6acb94bb354467b928
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-10-19 22:09:01 -0500

    comments-controller rspec passing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
-  def create
-	comment = Comment.create(content: params[:content], user: current_user, message: Message.find(params[:message_id]))
-  	redirect_to "/users/#{current_user.id}"  	
-  end
+	before_action :require_login
 
-  def destroy
-	Comment.destroy(params[:id])
-	redirect_to "/users/#{current_user.id}"  	
-  end
+	def create
+		comment = Comment.create(content: params[:content], user: current_user, message: Message.find(params[:message_id]))
+		redirect_to "/users/#{current_user.id}"  	
+	end
+
+	def destroy
+		Comment.destroy(params[:id])
+		redirect_to "/users/#{current_user.id}"  	
+	end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  before do
+    @user = create_user
+    @message = @user.messages.create(content: 'message')
+    @comment = Comment.create(user: @user, message: @message, content: 'comment')
+  end
+  describe "when not logged in" do
+  	it "does not allow you to create comments" do
+      post :create
+  		expect(response).to redirect_to(root_path)
+  	end
+  	it "does not allow you to delete comments" do
+  		delete :destroy, id: @comment
+      expect(response).to redirect_to(root_path)
+  	end
+  end
 end
